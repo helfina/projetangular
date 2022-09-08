20f17e7b1ce5418355aa134103049a009b9b44b5
Gaelle <gkerforne@gmail.com>
2022-09-08 11:39:28 +0200

    ajout des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {IndexShowPlanetComponent} from "./index-show-planet/index-show-planet.component";
+import {IndexShowPeopleComponent} from "./index-show-people/index-show-people.component";
+import {IndexShowVehiclesComponent} from "./index-show-vehicles/index-show-vehicles.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomePageComponent},
+  {path:'planets', component: IndexShowPlanetComponent},
+  {path:'people', component: IndexShowPeopleComponent},
+  {path:'vehicles', component: IndexShowVehiclesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
