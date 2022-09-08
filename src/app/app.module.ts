0bf37573a9004ea69a11e7e4fda58fec2208437d
Gaelle <gkerforne@gmail.com>
2022-09-08 11:38:26 +0200

    ajout du module http pour les requetes api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { IndexShowPlanetComponent } from './index-show-planet/index-show-planet.component';
+import {HttpClientModule} from "@angular/common/http";
+import { IndexShowPeopleComponent } from './index-show-people/index-show-people.component';
+import { IndexShowVehiclesComponent } from './index-show-vehicles/index-show-vehicles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    IndexShowPlanetComponent,
+    IndexShowPeopleComponent,
+    IndexShowVehiclesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
