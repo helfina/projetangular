de721391baf13b20e024e61c1c18af3a715d5b85
Gaelle <gkerforne@gmail.com>
2022-09-08 15:49:56 +0200

    affichage de la liste des planetes

--- src/app/index-show-planet/index-show-planet.component.ts
+++ src/app/index-show-planet/index-show-planet.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-index-show-planet',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index-show-planet.component.css']
 })
 export class IndexShowPlanetComponent implements OnInit {
+  detailsPlanets: any = [];
+  planets: any = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private httpService: HttpClient) {
   }
 
+  ngOnInit(): void {
+    this.getPlanets()
+  }
+
+  getPlanets() {
+    this.httpService
+      .get('https://swapi.dev/api/planets')
+      .subscribe(planets => {
+        this.detailsPlanets = planets;
+        if (this.detailsPlanets.next != null) {
+
+          let countPages = Math.ceil(this.detailsPlanets.count / 10);
+
+          for (let index = 1; index <= countPages; index++) {
+            this.httpService.get('https://swapi.dev/api/planets/?page=' + index).subscribe(pageOfPlanets => {
+              this.detailsPlanets = pageOfPlanets;
+              console.log(this.detailsPlanets)
+              this.detailsPlanets.results.forEach((result: any) => {
+                this.planets = this.planets.concat(result);
+
+              });
+            });
+          }
+
+        } else {
+
+          this.detailsPlanets.results.forEach((result: any) => {
+            this.planets = result;
+          });
+        }
+      });
+  }
 }
