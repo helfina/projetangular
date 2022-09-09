fb4cd8ebecc179f2abbe5cae842bbef69909ff71
Gaelle <gkerforne@gmail.com>
2022-09-09 09:17:24 +0200

    affichage de la liste des vehicles

--- src/app/index-show-vehicles/index-show-vehicles.component.ts
+++ src/app/index-show-vehicles/index-show-vehicles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-index-show-vehicles',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index-show-vehicles.component.css']
 })
 export class IndexShowVehiclesComponent implements OnInit {
-
-  constructor() { }
+  detailVehicles: any = [];
+  vehicles: any = [];
+  constructor(private httpService: HttpClient) { }
 
   ngOnInit(): void {
-  }
+    this.getVehicles();
 
+  }
+  getVehicles() {
+    this.httpService
+      .get('https://swapi.dev/api/vehicles')
+      .subscribe(vehicles => {
+        this.detailVehicles = vehicles;
+        if (this.detailVehicles.next != null) {
+
+          let countPages = Math.ceil(this.detailVehicles.count / 10);
+
+          for (let index = 1; index <= countPages; index++) {
+            this.httpService.get('https://swapi.dev/api/vehicles/?page=' + index).subscribe(pageOfVehicles => {
+              this.detailVehicles = pageOfVehicles;
+              console.log(this.detailVehicles)
+              this.detailVehicles.results.forEach((result: any) => {
+                this.vehicles = this.vehicles.concat(result);
+
+              });
+            });
+          }
+
+        } else {
+
+          this.detailVehicles.results.forEach((result: any) => {
+            this.vehicles = result;
+          });
+        }
+      });
+  }
 }
